63ccd52ac9643e565ca1cc189b1d57e84948ba72
cleber o gato      pindamonhagaba   o viagro   o perfurador de úteros <147511661+sttz32@users.noreply.github.com>
2025-04-23 09:34:00 -0300

    a

--- 1tri/projeto1/the beginning after the end/script.js
+++ 1tri/projeto1/the beginning after the end/script.js
@@ -32,6 +32,7 @@ document.addEventListener('DOMContentLoaded', function(){
  
      alternaContraste.addEventListener('click', function(){
          document.body.classList.toggle('alto-contraste')
+         document.body.classList
      })
  
  
